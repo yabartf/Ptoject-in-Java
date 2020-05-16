5ffb3f4a13b4c77223d9cf75930c018e8a965af1
dvirsivan1994@gmail.com <dvirsivan1994@gmail.com>
2020-05-16 23:15:25 +0300

    changed the geoPoint stuff

--- IME5780-E1-5840-6639/src/elements/AmbientLight.java
+++ IME5780-E1-5840-6639/src/elements/AmbientLight.java
@@ -6,7 +6,7 @@ public class AmbientLight {
     double  Ka;
     Color _intensity;
 
-    public AmbientLight(double ka, Color Ia) {
+    public AmbientLight(Color Ia,double ka) {
         Ka = ka;
         this._intensity = Ia.scale(Ka);
     }
