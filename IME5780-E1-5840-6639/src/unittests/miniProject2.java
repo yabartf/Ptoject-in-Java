56564ea6ef2abd9fcc90bef0d6f53a247de59891
yair <yabartf@gmail.com>
2020-06-24 15:24:48 +0300

    begin stage 5

--- IME5780-E1-5840-6639/src/unittests/miniProject2.java
+++ IME5780-E1-5840-6639/src/unittests/miniProject2.java
@@ -17,16 +17,17 @@ import scene.Scene;
 public class miniProject2 {
     @Test
     public void room(){
-        double height = 450;
-        double width = 2000;
-        double length = 2300;
+        double height = 850;
+        double width = 3000;
+        double length = 3300;
+        int tribunsSize = (int)width/10;
         Point3D rightUp = new Point3D(width/2,-length/2,0);
         Point3D rightDown = new Point3D(width/2,length/2,0);
         Point3D leftUp = new Point3D(-width/2,-length/2,0);
         Point3D leftDown = new Point3D(-width/2,length/2,0);
         Material wallsMaterial = new Material(0.5, 0.5, 60,0,0.3);
         Scene scene = new Scene("mini project2");
-        scene.setCamera(new Camera(new Point3D(0, length/2, -height*2), new Vector(0, -1, 1), new Vector(0, -1, -1),2));
+        scene.setCamera(new Camera(new Point3D(0, length/2, -height*1.3), new Vector(0, -1, 1), new Vector(0, -1, -1),2));
         scene.setViewPlaneDistance(50);
         scene.setFocalPlaneDistance(180);
         scene.setBackground(Color.BLACK);
@@ -35,9 +36,17 @@ public class miniProject2 {
         Square rightWall = new Square(new Color(java.awt.Color.BLUE),wallsMaterial,rightDown,rightUp,new Point3D(rightDown.get_x(),rightDown.get_y(),-height));
         Square leftWall = new Square(new Color(java.awt.Color.RED),wallsMaterial,leftDown,leftUp,new Point3D(leftDown.get_x(),leftDown.get_y(),-height));
         Square floor = new Square(new Color(java.awt.Color.YELLOW),wallsMaterial,leftDown,leftUp,rightDown);
-        rightTribune(rightDown,rightUp,300,height, scene);
-        leftTribune(leftDown,leftUp,300,height, scene);
-        scene.addGeometries(forwardWall,rightWall,leftWall,floor);
+        Square[] borders = new Square[4];
+        borders[0] = new Square(Color.BLACK,wallsMaterial,new Point3D(rightUp.get_x()-tribunsSize-80,rightUp.get_y()-50,-1),
+                new Point3D(rightDown.get_x()-tribunsSize-80,rightDown.get_y()+50,-1),
+                new Point3D(rightUp.get_x()-tribunsSize-150,rightUp.get_y()-50,-1));
+        borders[1] = new Square(Color.BLACK,wallsMaterial,new Point3D(leftUp.get_x()+tribunsSize+80,leftUp.get_y()-50,-1),
+                new Point3D(leftDown.get_x()+tribunsSize+80,leftDown.get_y()+50,-1),
+                new Point3D(leftUp.get_x()+tribunsSize+150,leftUp.get_y()-50,-1));
+
+        rightTribune(rightDown,rightUp,tribunsSize,height, scene);
+        leftTribune(leftDown,leftUp,tribunsSize,height, scene);
+        scene.addGeometries(forwardWall,rightWall,leftWall,floor,borders[0],borders[1]);
         ImageWriter imageWriter = new ImageWriter("room", 200, 200, 600, 600);
         Render render = new Render(imageWriter, scene).setMultithreading(3).setDebugPrint();
 
@@ -51,13 +60,18 @@ public class miniProject2 {
         Material tMaterial = new Material(0.5, 0.5, 60,0,0.3);
         int someTribuns = size/20;
         Square[] tribuns = new Square[someTribuns];
-
+        Square[] benchs = new Square[someTribuns];
         for(int i = 0; i < someTribuns; i++) {
             try {
+                Point3D temp = new Point3D(rightUp.get_x() - i * size/someTribuns, rightUp.get_y(), - height + (i * height/someTribuns));
+
                 tribuns[i] = new Square(tColor, tMaterial, new Point3D(rightUp.get_x(), rightUp.get_y(), - height + (i * height/someTribuns)),//
                         new Point3D(rightDown.get_x(), rightDown.get_y(), - height + (i * height/someTribuns)),//
-                        new Point3D(rightUp.get_x() - i * size/someTribuns, rightUp.get_y(), - height + (i * height/someTribuns)));
-                scene.addGeometries(tribuns[i]);
+                        temp);
+                benchs[i] = new Square(Color.BLACK,tMaterial,temp,
+                        tribuns[i].fourthPoint,
+                        new Point3D(rightUp.get_x()- i * size/someTribuns,rightUp.get_y(),- height + ((1+i) * height/someTribuns)));
+                scene.addGeometries(tribuns[i],benchs[i]);
             }
             catch (IllegalArgumentException e){
                 if(e.getMessage() != "zero vector")
@@ -71,13 +85,19 @@ public class miniProject2 {
         Material tMaterial = new Material(0.5, 0.5, 60,0,0.3);
         int someTribuns = size/20;
         Square[] tribuns = new Square[someTribuns];
+        Square[] benchs = new Square[someTribuns];
 
         for(int i = 0; i < someTribuns; i++) {
             try {
+                Point3D temp = new Point3D(leftUp.get_x() + i * size/someTribuns, leftUp.get_y(), - height + (i * height/someTribuns));
+
                 tribuns[i] = new Square(tColor, tMaterial, new Point3D(leftUp.get_x(), leftUp.get_y(), - height + (i * height/someTribuns)),//
                         new Point3D(leftDown.get_x(), leftDown.get_y(), - height + (i * height/someTribuns)),//
-                        new Point3D(leftUp.get_x() + i * size/someTribuns, leftUp.get_y(), - height + (i * height/someTribuns)));
-                scene.addGeometries(tribuns[i]);
+                        temp);
+                benchs[i] = new Square(Color.BLACK, tMaterial, new Point3D(leftDown.get_x() + i * size/someTribuns, leftDown.get_y(), - height + (i * height/someTribuns)),//
+                        new Point3D(leftDown.get_x() + i * size/someTribuns, leftDown.get_y(), - height + ((1+i) * height/( someTribuns))),//
+                        temp);
+                scene.addGeometries(tribuns[i],benchs[i]);
             }
             catch (IllegalArgumentException e){
                 if(e.getMessage() != "zero vector")
